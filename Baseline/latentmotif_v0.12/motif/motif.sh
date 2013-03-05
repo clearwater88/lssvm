4a0001a97f7e4829f8b4eb1b583ebc113dbd70cc
clearwater88 <clearwater88@gmail.com>
2013-03-05 10:34:23 -0600

    scripts to run

---
+++ Baseline/latentmotif_v0.12/motif/motif.sh
@@ -0,0 +1,2 @@
+./svm_motif_learn -c 150 --m 8 --o 3 -b 0 parser_data/res_Alx3_3418.2_deBruijn.txt example.model
+./svm_motif_classify --m 8 --o 3 parser_data/res_Dlx1_1741.2_deBruijn.txt example.model
